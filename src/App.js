a90b4b8cd45f29537dfdd9174d4cef69d6102edf
Renz 8:28 <110266073+RenzKing@users.noreply.github.com>
2023-06-30 19:06:43 +0800

    state

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const messages = [
   "Learn React ⚛️",
   "Apply for jobs 💼",
@@ -5,14 +7,14 @@ const messages = [
 ];
 
 export default function App() {
-  const step = 1;
+  const [step, setStep] = useState(1);
 
   function handlePrevious() {
     alert("Previous");
   }
 
   function handleNext() {
-    alert("Next");
+    setStep(step + 1);
   }
 
   return (
@@ -22,7 +24,7 @@ export default function App() {
 
         <div className={`${step >= 2 ? "active" : ""}`}>2</div>
 
-        <div className={`${step} >=3 ? "active" : ""`}>3</div>
+        <div className={`${step >= 3 ? "active" : ""}`}>3</div>
       </div>
 
       <p className="message">
@@ -32,14 +34,14 @@ export default function App() {
       <div className="buttons">
         <button
           style={{ backgroundColor: "#7950f2", color: "#fff" }}
-          onClick={handlePrevious()}
+          onClick={handlePrevious}
         >
           Previous
         </button>
 
         <button
           style={{ backgroundColor: "#7950f2", color: "#fff" }}
-          onClick={handleNext()}
+          onClick={handleNext}
         >
           Next
         </button>
